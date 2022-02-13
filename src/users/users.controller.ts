e86d9ea810e501187f5a617d6bb83a51f9a87cfe
KacperKluska <kacperkluska130@gmail.com>
2022-02-13 16:38:38 +0100

    Fixed user controller with 1 route and 1 service method for taking out user data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,14 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { newUserDTO, userLoginDTO } from './dto/userAuth';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
-  @Get('hello')
-  helloUser(@Query() query: { email }) {
-    return { msg: `Hello ${query.email}` };
-  }
-
-  @UseGuards(JwtAuthGuard)
   @Get('user')
-  async findUser(@Query() query: { email }) {
-    return await this.userService.findOneByEmail(query.email);
+  @UseGuards(JwtAuthGuard)
+  async findUser(@Req() request) {
+    return await this.userService.findOneByEmail(request.user.email);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,30 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/entities/user.entity';
 import { UserRolesService } from 'src/user-roles/user-roles.service';
-import { Response } from 'src/shared_dto/response';
 
 @Injectable()
 export class UsersService {
   constructor(private userRolesService: UserRolesService) {}
 
-  findOneByEmail(email: string): Promise<User | undefined> {
-    // return User.find({
-    //   relations: ['userRole'],
-    //   where: {
-    //     email,
-    //   },
-    // });
+  async findOneByEmail(email: string): Promise<User | undefined> {
+    const users = await User.find({
+      relations: ['userRole'],
+      where: {
+        email,
+      },
+    });
 
-    // return User.find({ relations: ['userRole'] });
-    return User.findOne({ email });
-
-    // return User.find({
-    //   join: {
-    //     alias: 'user',
-    //     leftJoinAndSelect: {
-    //       userRole: 'user.userRole',
-    //     },
-    //   },
-    // });
+    return users[0];
   }
 }
