47e8df157ddc844007aa8448c142ea88f24c6cf3
KacperKluska <kacperkluska130@gmail.com>
2022-05-28 18:09:41 +0200

    added workoutSession controller and service with POST and GET methods

--- src/workout-sessions/workout-sessions.module.ts
+++ src/workout-sessions/workout-sessions.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WorkoutSession } from 'src/entities/workout-session.entity';
+import { UsersModule } from 'src/users/users.module';
+import { WorkoutSessionsController } from './workout-sessions.controller';
+import { WorkoutSessionsService } from './workout-sessions.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WorkoutSession])],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([WorkoutSession]), UsersModule],
+  controllers: [WorkoutSessionsController],
+  providers: [WorkoutSessionsService],
+  exports: [WorkoutSessionsService],
 })
 export class WorkoutSessionsModule {}
