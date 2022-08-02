16c817b11648e0580c2a80eeca095341d6fc8723
KacperKluska <kacperkluska130@gmail.com>
2022-08-02 21:54:40 +0200

    refactored creating workout session

--- src/workout-sessions/workout-sessions.controller.ts
+++ src/workout-sessions/workout-sessions.controller.ts
@@ -1,19 +1,23 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Res } from '@nestjs/common';
 import { WorkoutSessionsService } from './workout-sessions.service';
+import { Response } from 'express';
 
 @Controller('workout-sessions')
 export class WorkoutSessionsController {
   // TODO create endpoints for GET (by date)
   constructor(private workoutSessionService: WorkoutSessionsService) {}
 
+  // TODO refactor others like that one
   @Post()
   async createWorkoutSession(
     @Body() body: { swimmerEmail: string; coachEmail: string },
+    @Res() res: Response,
   ) {
-    return await this.workoutSessionService.createWorkoutSession(
+    const result = await this.workoutSessionService.createWorkoutSession(
       body.swimmerEmail,
       body.coachEmail,
     );
+    res.status(result.status).send({ message: result.message, ...result.data });
   }
 
   @Get('forSwimmer')
--- src/workout-sessions/workout-sessions.service.ts
+++ src/workout-sessions/workout-sessions.service.ts
@@ -7,23 +7,36 @@ export class WorkoutSessionsService {
   constructor(private usersService: UsersService) {}
 
   async createWorkoutSession(swimmerEmail: string, coachEmail: string) {
-    if (swimmerEmail === coachEmail)
-      return { status: 400, message: 'Emails are equal!' };
-    const swimmer = await this.usersService.findOneByEmail(swimmerEmail);
-    const coach = await this.usersService.findOneByEmail(coachEmail);
+    try {
+      if (swimmerEmail === coachEmail)
+        return { status: 400, message: 'Emails are equal!' };
+      const swimmer = await this.usersService.findOneByEmail(swimmerEmail);
+      const coach = await this.usersService.findOneByEmail(coachEmail);
 
-    if (
-      !coach ||
-      !swimmer ||
-      coach.userRole.role !== 'COACH' ||
-      swimmer.userRole.role !== 'USER'
-    )
-      return { status: 400, message: `You provided incorrect emails.` };
+      if (
+        !coach ||
+        !swimmer ||
+        coach.userRole.role !== 'COACH' ||
+        swimmer.userRole.role !== 'USER'
+      )
+        return { status: 400, message: `You provided incorrect emails.` };
 
-    const currentDate = new Date();
-    const workoutSession = new WorkoutSession(swimmer, coach, currentDate);
-    await WorkoutSession.save(workoutSession);
-    return { status: 201, message: `Successfully created workout session.` };
+      const currentDate = new Date();
+      const workoutSession = new WorkoutSession(swimmer, coach, currentDate);
+      const result = await WorkoutSession.save(workoutSession);
+
+      return {
+        status: 201,
+        message: `Successfully created workout session.`,
+        data: result,
+      };
+    } catch (error) {
+      return {
+        status: 500,
+        message: `Couldn't create workout session.`,
+        data: error,
+      };
+    }
   }
 
   async getSwimmerWorkoutSessions(swimmerEmail: string) {
