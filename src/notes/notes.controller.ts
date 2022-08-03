bca9b364d5c0274b2f2f4af49f3985f2db53eb50
KacperKluska <kacperkluska130@gmail.com>
2022-08-03 14:34:22 +0200

    refactored notes module

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -7,9 +7,11 @@ import {
   Patch,
   Post,
   Query,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import { NotesService } from './notes.service';
+import { Response } from 'express';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { JwtAuthGuard, RolesGuard } from 'src/auth/guards';
 import { Role } from 'src/auth/utils/roles.enum';
@@ -21,29 +23,34 @@ export class NotesController {
   @Post()
   @Roles(Role.COACH)
   @UseGuards(JwtAuthGuard, RolesGuard)
-  async createNote(@Body() body: { text: string; workoutSessionId: string }) {
-    return this.notesService.createNote(body.text, body.workoutSessionId);
+  async createNote(
+    @Body() body: { text: string; workoutSessionId: string },
+    @Res() res: Response,
+  ) {
+    const result = await this.notesService.createNote(
+      body.text,
+      body.workoutSessionId,
+    );
+    res.status(result.status).send({ message: result.message, ...result.data });
   }
 
   @Delete()
   @Roles(Role.COACH)
   @UseGuards(JwtAuthGuard, RolesGuard)
-  async deleteNote(@Body() body: { noteId: string }) {
-    return this.notesService.deleteNote(body.noteId);
+  async deleteNote(@Body() body: { noteId: string }, @Res() res: Response) {
+    const result = await this.notesService.deleteNote(body.noteId);
+    res.status(result.status).send({ message: result.message, ...result.data });
   }
 
   @Patch()
   @Roles(Role.COACH)
   @UseGuards(JwtAuthGuard, RolesGuard)
-  async updateNote(@Body() body: { text: string; noteId: string }) {
-    return this.notesService.updateNote(body.text, body.noteId);
-  }
-
-  @Get('/:id')
-  @Roles(Role.COACH, Role.USER)
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  async getNoteById(@Param('id') id: string) {
-    return this.notesService.getNoteByNoteId(id);
+  async updateNote(
+    @Body() body: { text: string; noteId: string },
+    @Res() res: Response,
+  ) {
+    const result = await this.notesService.updateNote(body.text, body.noteId);
+    res.status(result.status).send({ message: result.message, ...result.data });
   }
 
   @Get()
@@ -51,9 +58,11 @@ export class NotesController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   async getNotesByWorkoutSessionId(
     @Query() query: { workoutSessionId: string },
+    @Res() res: Response,
   ) {
-    return this.notesService.getAllNotesForWorkoutSession(
+    const result = await this.notesService.getAllNotesForWorkoutSession(
       query.workoutSessionId,
     );
+    res.status(result.status).send({ message: result.message, ...result.data });
   }
 }
--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,47 +1,100 @@
 import { Injectable } from '@nestjs/common';
 import { Note } from 'src/entities/note.entity';
+import { MyResponse } from 'src/shared_dto/response';
 import { WorkoutSessionsService } from 'src/workout-sessions/workout-sessions.service';
+import { serverErrorResponse } from 'src/shared_dto/error';
 
 @Injectable()
 export class NotesService {
   constructor(private workoutSessionsService: WorkoutSessionsService) {}
 
-  async createNote(text: string, workoutSessionId: string) {
-    const workoutSession =
-      await this.workoutSessionsService.getWorkoutSessionById(workoutSessionId);
-    const note = new Note(text, workoutSession);
-    if (!note) return `Couldn't create a note.`;
-    return await Note.save(note);
+  async createNote(
+    text: string,
+    workoutSessionId: string,
+  ): Promise<MyResponse> {
+    try {
+      const workoutSession =
+        await this.workoutSessionsService.getWorkoutSessionById(
+          workoutSessionId,
+        );
+      const note = new Note(text, workoutSession);
+      if (!note)
+        return {
+          status: 400,
+          message: `Couldn't create a note.`,
+        };
+      const result = await Note.save(note);
+      return {
+        status: 201,
+        message: `Note created.`,
+        data: result,
+      };
+    } catch (error) {
+      return serverErrorResponse(error);
+    }
   }
 
-  async deleteNote(noteId: string) {
-    const note = await this.getNoteByNoteId(noteId);
-    if (!note) return `Couldn't find a note.`;
-    await Note.remove(note);
-    return 'Note removed';
+  async deleteNote(noteId: string): Promise<MyResponse> {
+    try {
+      const note = await this.getNoteByNoteId(noteId);
+      if (!note)
+        return {
+          status: 400,
+          message: `Couldn't find a note.`,
+        };
+      await Note.remove(note);
+      return {
+        status: 200,
+        message: 'Note removed',
+      };
+    } catch (error) {
+      return serverErrorResponse(error);
+    }
   }
 
-  async updateNote(newText: string, noteId: string) {
-    const note = await this.getNoteByNoteId(noteId);
-    if (!note) return `Couldn't find a note.`;
+  async updateNote(newText: string, noteId: string): Promise<MyResponse> {
+    try {
+      const note = await this.getNoteByNoteId(noteId);
+      if (!note)
+        return {
+          status: 400,
+          message: `Couldn't find a note.`,
+        };
 
-    note.note = newText;
-    return Note.save(note);
+      note.note = newText;
+      const result = await Note.save(note);
+      return { status: 201, message: 'Note updated', data: { note: result } };
+    } catch (error) {
+      return serverErrorResponse(error);
+    }
+  }
+
+  async getAllNotesForWorkoutSession(
+    workoutSessionId: string,
+  ): Promise<MyResponse> {
+    try {
+      const workoutSession =
+        await this.workoutSessionsService.getWorkoutSessionById(
+          workoutSessionId,
+        );
+      const notes = await Note.find({
+        relations: ['workoutSession'],
+        where: { workoutSession: workoutSession },
+      });
+      return { status: 200, message: 'Success', data: { notes } };
+    } catch (error) {
+      return serverErrorResponse(error);
+    }
   }
 
   async getNoteByNoteId(noteId: string) {
-    return await Note.findOne({
-      relations: ['workoutSession'],
-      where: { id: noteId },
-    });
-  }
-
-  async getAllNotesForWorkoutSession(workoutSessionId: string) {
-    const workoutSession =
-      await this.workoutSessionsService.getWorkoutSessionById(workoutSessionId);
-    return await Note.find({
-      relations: ['workoutSession'],
-      where: { workoutSession: workoutSession },
-    });
+    try {
+      return await Note.findOne({
+        relations: ['workoutSession'],
+        where: { id: noteId },
+      });
+    } catch (error) {
+      return null;
+    }
   }
 }
